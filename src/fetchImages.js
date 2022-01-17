b5afa0753a8a05ccb8a8cd2445e50fb7101289cd
Yaroslav Semenov <yaryk2013@gmail.com>
2022-01-17 23:10:28 +0200

    completed main task

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,14 +1,39 @@
-export { fetchImages };
+export { fetchImages, incrementPage, setQuery, resetPage, getQuery };
 
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '25274192-4013926bd09e77e2599d4477f';
 const QUERY_PARAMETERS = 'image_type=photo&orientation=horizontal&safesearch=true';
+const PER_PAGE = 'per_page=40';
+let searchQuery = '';
+let page = 1;
 
 function fetchImages(searchQuery) {
-  return fetch(`${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&${QUERY_PARAMETERS}`).then(res => {
+  return fetch(
+    `${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&${QUERY_PARAMETERS}&page=${page}&${PER_PAGE}`,
+  ).then(res => {
     if (!res.ok) {
       throw new Error(res.status);
     }
     return res.json();
   });
+}
+
+function getQuery() {
+  return searchQuery;
+}
+
+function setQuery(newQuery) {
+  searchQuery = newQuery;
+}
+
+function incrementPage() {
+  page += 1;
+}
+
+function resetPage() {
+  page = 1;
+}
+
+function getPage() {
+  console.log(page);
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
-import { fetchImages } from './fetchImages';
+import { fetchImages, incrementPage, setQuery, resetPage, getQuery } from './fetchImages';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let gallery = new SimpleLightbox('.gallery a');
 
 form.addEventListener('submit', OnSubmitForm);
+loadMoreBtn.addEventListener('click', fetchNextPage);
 
 function OnSubmitForm(e) {
   e.preventDefault();
+  loadMoreBtn.classList.add('visually-hidden');
 
   const searchQuery = e.currentTarget.elements.searchQuery.value;
 
+  setQuery(searchQuery);
+  resetPage();
+
   fetchImages(searchQuery)
     .then(res => {
+      Notify.success(`Hooray! We found ${res.totalHits} images.`);
       galleryEl.innerHTML = '';
 
       const arrImg = res.hits;
@@ -25,44 +34,72 @@ function OnSubmitForm(e) {
         );
       }
 
-      const markup = arrImg
-        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-          return `
+      renderCard(arrImg);
+      runSimpleLightBox();
+      loadMoreBtn.classList.remove('visually-hidden');
+    })
+    .catch(error => console.log(error));
+}
+
+function renderCard(arrImg) {
+  const markup = arrImg
+    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return `
         <a class="photo-card" href='${largeImageURL}'>
-          <img class="gallery-img"
-              src="${webformatURL} width="277" height="194""
-              alt="${tags}"
-              loading="lazy" />
+          <img src="${webformatURL} width="295" height="195""
+               alt="${tags}"
+               loading="lazy" />
           <div class="info">
             <p class="info-item">
-              <b>Likes</b>
+              <b class="info-title">Likes</b>
               <span>${likes}</span>
             </p>
             <p class="info-item">
-              <b>Views</b>
+              <b class="info-title">Views</b>
               <span>${views}</span>
             </p>
             <p class="info-item">
-              <b>Comments</b>
+              <b class="info-title">Comments</b>
               <span>${comments}</span>
             </p>
             <p class="info-item">
-              <b>Downloads</b>
+              <b class="info-title">Downloads</b>
               <span>${downloads}</span>
             </p>
           </div>
         </a>
         `;
-        })
-        .join('');
+    })
+    .join('');
 
-      galleryEl.insertAdjacentHTML('beforeend', markup);
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
 
-      let gallery = new SimpleLightbox('.gallery a');
-      gallery.on('show.simplelightbox', function () {
-        gallery.options.captionsData = 'alt';
-        gallery.options.captionDelay = 250;
-      });
+function fetchNextPage() {
+  incrementPage();
+
+  const searchQuery = getQuery();
+
+  fetchImages(searchQuery)
+    .then(res => {
+      const arrImg = res.hits;
+      if (arrImg.length === 0) {
+        return Notify.info("We're sorry, but you've reached the end of search results.");
+      }
+
+      renderCard(res.hits);
+      refreshSimpleLightBox();
     })
     .catch(error => console.log(error));
+}
+
+function runSimpleLightBox() {
+  gallery.on('show.simplelightbox', function () {
+    gallery.options.captionsData = 'alt';
+    gallery.options.captionDelay = 250;
+  });
+}
+
+function refreshSimpleLightBox() {
+  gallery.refresh();
 }
